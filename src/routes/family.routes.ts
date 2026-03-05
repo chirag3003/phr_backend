a599edf8091e056106678aa183de038055c8fb7d
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2026-03-05 12:42:26 +0530

    updates

--- src/routes/family.routes.ts
+++ src/routes/family.routes.ts
@@ -21,7 +21,7 @@ familyRoutes.post("/:id/leave", authMiddleware, familyController.leaveFamily);
 // Permission management
 familyRoutes.get("/permissions", authMiddleware, familyController.getPermissionEntry);
 familyRoutes.post("/permissions", authMiddleware, familyController.createPermissionEntry);
-familyRoutes.put("/:id/permissions", authMiddleware, familyController.updatePermissionEntry);
-familyRoutes.delete("/:id/permissions", authMiddleware, familyController.deletePermissionEntry);
+familyRoutes.put("/permissions", authMiddleware, familyController.updatePermissionEntry);
+familyRoutes.delete("/permissions", authMiddleware, familyController.deletePermissionEntry);
 
 export default familyRoutes;
